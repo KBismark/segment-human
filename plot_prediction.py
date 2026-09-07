ab32b64321f63e27ff4524b2b19aec714c91d711
kbismark <bismarkkwabenayamoah@gmail.com>
2026-09-07 13:14:40 +0000

    test(plot): update PennFudan sample selection for prediction script

--- plot_prediction.py
+++ plot_prediction.py
@@ -31,17 +31,7 @@ print("Loaded fine-tuned weights.")
 
 
 sample1 = pennfudan_dataset[0]  
-sample2 = pennfudan_dataset[51]  
-test_samples = [
-    {'image': sample1['image'], 'mask': sample1['mask'], 'name': "PennFudan Sample 1"},
-    {'image': sample2['image'], 'mask': sample2['mask'], 'name': "PennFudan Sample 2"}
-]
-
-# Visualization 
-visualize_segmentation_comparison(model, run_yolo_unet_pipeline, test_samples, save_path="results/penn_fudan.png")
-
-sample1 = pennfudan_dataset[0]  
-sample2 = pennfudan_dataset[50]  
+sample2 = pennfudan_dataset[57]  
 test_samples = [
     {'image': sample1['image'], 'mask': sample1['mask'], 'name': "PennFudan Sample 1"},
     {'image': sample2['image'], 'mask': sample2['mask'], 'name': "PennFudan Sample 2"}
